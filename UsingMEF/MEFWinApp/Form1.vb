46400e9f3a48d6f885dc65d0bf938da3e7d0da6c
Jorge Machado <justelnegro@gmail.com>
2012-04-11 08:33:29 -0430

    colocando un comentario para probar GIT

--- UsingMEF/MEFWinApp/Form1.vb
+++ UsingMEF/MEFWinApp/Form1.vb
@@ -3,6 +3,11 @@
     Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
         Dim p As New Program
         p.Run()
+        'esto es una prueba de cambios para probar GIT
         MessageBox.Show(String.Format("El calculo es:  {0}", p.CalculoNacionalizado.Calculo(2)).ToString())
+    End Sub
+
+    Private Sub Form1_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+
     End Sub
 End Class
